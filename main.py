1ae05ed7d25c25dc92f3c231a96e8775c0c1f494
Kaushal <kaushal.deokar@gmail.com>
2023-11-22 14:44:52 +0530

    tested and image upload account to client name

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 import os
-
+from typing import Any, List,Dict
 app = FastAPI()
 
 
-
-UPLOAD_FOLDER = "uploads"
-
+# change url accordingly
+UPLOAD_FOLDER = "D:\TIE\Backend\FAST_API_ENV"
+UPLOAD_FOLDER=os.path.join(UPLOAD_FOLDER, "uploads")
 
 def create_folder(folder_name):
     folder_path = os.path.join(UPLOAD_FOLDER, folder_name)
@@ -15,11 +15,18 @@ def create_folder(folder_name):
     return folder_path
 
 @app.post("/upload")
-async def upload_image(payload: str, file: UploadFile = File(...)):
-    folder_path = create_folder(payload)
+async def upload_image(client_name:str,Bilty_No:str, file: UploadFile = File(...)):
+
+    folder_path = create_folder(client_name)
+    print(file.filename)
+    # change file name on basis of extension
+    # file.filename=file.filename+Bilty_No
     file_path = os.path.join(folder_path, file.filename)
 
     with open(file_path, "wb") as image:
         image.write(file.file.read())
 
     return {"message": "Image uploaded successfully", "file_path": file_path}
+
+
+
